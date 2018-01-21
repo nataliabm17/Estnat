89739dd0d2c56b6f66a33faae5fd7f83687373b4
Natalia Bolanos Murillo <natibm17@gmail.com>
2018-01-21 05:05:09 -0600

    general

--- test/general.py
+++ test/general.py
@@ -4,7 +4,17 @@ from pygame.locals import *
 import serial
 import time
 
+
+
+import pylab
+import matplotlib
+matplotlib.use("Agg")
+import matplotlib.backends.backend_agg as agg
+from matplotlib import style
+
+style.use("ggplot")
 green = (32,178,170)
+lime = (0, 255, 0)
 red = (200, 0 ,0)
 black = (0,0,0)
 white = (255,255,255)
@@ -25,21 +35,59 @@ def message_display(text, x, y, size):
     TextRect.center = (x,y)
     window.blit(TextSurf, TextRect)
 
+def display_general(color1, text):
+    pygame.draw.rect(screen, color1,(200,70,600,100))
+    message_display(text, 500, 110, 40)
+
+    message_display('Team #: 1230', 200, 260, 25)
+    message_display('Mission Time:', 200, 320, 25)
+    message_display('Heatshield:', 200, 380, 25)
+    message_display('Parachute:', 200, 500, 25)
+
+    pygame.draw.rect(screen, lime,(475,775,250,50))
+    pygame.draw.rect(screen, red,(250,775,100,50))
+    message_display('Save and export', 600, 800, 25)
+    message_display('Reset', 300, 800, 25)
+
+    message_display('Tilt:', 600, 260, 25)
+    message_display('Height:', 600, 320, 25)
+    message_display('Voltage:', 600, 380, 25)
+    message_display('Pressure:', 600, 440, 25)
+    message_display('Temperature:', 600, 500, 25)
+    message_display('Acceleration:', 600, 560, 25)
+
 
 def button(x, y, height, width, color1, color2, text, text_size, position):
-
+    click = pygame.mouse.get_pressed()
     mouse = pygame.mouse.get_pos()
     if (x+width > mouse[0] > x and (y*position)+height > mouse[1] > y*position):
         pygame.draw.rect(screen, color2,(x,y*position,width,height))
+
+        if click[0] == 1:
+            if position == 1:
+               display_general(color1, text)
+
+            else:
+                pygame.draw.rect(screen, color1,(200,70,600,100))
+                message_display(text, 500, 110, 40)
+
+                fig = pylab.figure(figsize=[8, 6.5], # Inches
+                                   dpi=100,        # 100 dots per inch, so the resulting buffer is 400x400 pixels
+                                   )
+                ax = fig.gca()
+                ax.plot([1, 2, 4, 8,10])
+                canvas = agg.FigureCanvasAgg(fig)
+                canvas.draw()
+                renderer = canvas.get_renderer()
+                raw_data = renderer.tostring_rgb()
+                surf = pygame.image.fromstring(raw_data, (800,650), "RGB")
+                screen.blit(surf, (120,230))
+                pygame.display.flip()
+
     else:
         pygame.draw.rect(screen, color1,(x,y*position,width,height))
 
     message_display(text, (x + width/2), (y*position + height/2), text_size)
-
-    click = pygame.mouse.get_pressed()
-    if click[0] == 1:
-        pygame.draw.rect(screen, color1,(200,100,600,100))
-        message_display(text, 500, 150, 40)
 
 
 def create():
@@ -55,7 +103,7 @@ def create():
 
 def game_loop():
 
-    pygame.display.set_caption('U sure')
+    pygame.display.set_caption('U sure CANSAT UCR')
 
     background_colour = light_blue
 
